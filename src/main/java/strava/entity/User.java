ed7dca9cc6eed31104f3dbe33d4b8d967061d09e
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2024-12-11 10:25:36 +0100

    implementación jakarta al usuario

--- src/main/java/strava/entity/User.java
+++ src/main/java/strava/entity/User.java
@@ -1,57 +1,69 @@
 package strava.entity;
 
 import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Table(name="userTable")
+@Entity
 public class User {
-	 private Long id;
-	 private String password;
-	 private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String password;
+	@Column(unique = true)
+	private String email;
 	    
-	 public User() {
+	public User() {
 		 
-	 }
+	}
 	    
-	 public User(String email, String password) {
-		 this.email = email;
-		 this.password = password;
-	 }
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
 
-	 public String getEmail() {
-		 return email;
-	 }
+	public String getEmail() {
+		return email;
+	}
 
-	 public void setEmail(String email) {
-		 this.email = email;
-	 }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	 public String getPassword() {
-		 return password;
-	 }
+	public String getPassword() {
+		return password;
+	}
 
-	 public void setPassword(String password) {
-		 this.password = password;
-	 }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-	 @Override
-	 public String toString() {
-		 return "User [id=" + id + ", email=" + email + ", password=" + password + "]";
-	 }
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
 
-	 @Override
-	 public int hashCode() {
-		 return Objects.hash(email, id, password);
-	 }
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, password);
+	}
 
-	 @Override
-	 public boolean equals(Object obj) {
-		 if (this == obj)
-			 return true;
-		 if (obj == null)
-			 return false;
-		 if (getClass() != obj.getClass())
-			 return false;
-		 User other = (User) obj;
-		 return Objects.equals(email, other.email) && Objects.equals(id, other.id)
-				 && Objects.equals(password, other.password);
-		}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(password, other.password);
+	}
 }
